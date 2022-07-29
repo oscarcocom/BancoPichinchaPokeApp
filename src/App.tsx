fc02e0049cd34d854ee53b5558168a87351fcd4c
Ing Osmar <oscarcocom11@gmail.com>
2022-07-28 21:37:53 -0500

    Upload design

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,131 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./css/App.css";
+import { FiSearch } from "react-icons/fi";
+import { MdAdd } from "react-icons/md";
+import { AiOutlineEdit } from "react-icons/ai";
+import { MdDeleteForever,MdOutlineSave } from "react-icons/md";
+import { RiCloseLine } from "react-icons/ri";
 
+
+
+import PokeRange from "./Components/Pages/UIMenu/PokeRange";
 function App() {
+  const [Ataque, setAtaque] = useState(50);
+  const [Defensa, setDefensa] = useState(50);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="container">
+        <div className="header">
+          <div className="SearchBox columna-3 columna-s-12">
+            <label htmlFor="labelSearch">Listado de pokemones</label>
+
+            <div className="InputSearchContent">
+              <input type="text" placeholder="Buscar" />
+              <FiSearch className="iconSearch" />
+            </div>
+          </div>
+
+          <div className="ButtonBox columna-3 columna-s-12">
+            <button>
+              <MdAdd className="icons" />
+              <span>Nuevo</span>
+            </button>
+          </div>
+        </div>
+
+        <div className="table">
+          <table>
+            <tr>
+              <th>Nombre</th>
+              <th>Lastname</th>
+              <th>Ataque</th>
+              <th>Defensa</th>
+              <th>Acciones</th>
+            </tr>
+            <tr>
+              <td>lvunsue</td>
+              <td>
+                <img />
+              </td>
+              <td>65</td>
+              <td>38</td>
+              <td className="CrudTable">
+                <span>
+                  <AiOutlineEdit />
+                </span>
+                <span>
+                  <MdDeleteForever />
+                </span>
+              </td>
+            </tr>
+          </table>
+        </div>
+        <div className="AddPokemon">
+          <div className="fila">
+            <div className="titleAddBox columna-s-12 columna-6">
+              <h4>Nuevo Pokemon</h4>
+            </div>
+          </div>
+          <div className="fila">
+            {/* inputs */}
+            <div className="inputsBox columna-s-12 columna-8">
+              {/* Nombre */}
+
+              <div className="NombreAdd  columna-s-12 columnna-6">
+                <label htmlFor="Nombre">Nombre:</label>
+                <input placeholder="Ingresa nombre" />
+              </div>
+              <div className="ImgAdd  columna-s-12 columnna-6">
+                <label htmlFor="Nombre">Imagen:</label>
+                <input placeholder="url" />
+              </div>
+
+              {/* url */}
+            </div>
+
+            {/* ranges */}
+            <div className="RangesBox columna-s-12 columna-8">
+             
+              <div className="ataque">
+
+                <label htmlFor="">Ataque: 0 </label>
+                <PokeRange
+                  value={Ataque}
+                  onChange={(e: any) => setAtaque(e.target.value)}
+                />{" "}
+                <label htmlFor="">100</label>
+              </div>
+
+              <div className="defensa ">
+                <label htmlFor="">Defensa: 0 </label>
+                <PokeRange
+                  value={Defensa}
+                  onChange={(e: any) => setDefensa(e.target.value)}
+                />{" "}
+                <label htmlFor="">100</label>
+              </div>
+            </div>
+          </div>
+
+          <div className="Footer fila">
+            <div className="ButtonBoxFooter columna-8 columna-s-8">
+
+          
+            <button>
+              <MdOutlineSave className="icons" />
+              <span>Guardar</span>
+            </button>
+            <button>
+              <RiCloseLine className="icons" />
+              <span>Cancelar</span>
+            </button>
+          
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
---
+++ src/Components/Pages/UIMenu/PokeRange.tsx
@@ -0,0 +1,34 @@
+import styled from 'styled-components'
+
+type propsRange= any;
+const PokeRange = styled.input.attrs({ type: 'range' })`
+  -webkit-appearance: none;
+  -moz-appearance: none;
+  outline: 0;
+  height: 7px;
+  width: 13rem;
+  border-radius: 40px;
+  background: ${(props:propsRange) =>
+    `linear-gradient(to right, rgb(89, 31, 196) 0%, #6f00ff ${props.value}%, #977fcc9e ${props.value}%, #977fcc85  0%);`};
+  box-shadow: 0px 0px 4px rgba(0, 0, 0, 0.0);
+
+  ::-webkit-slider-thumb {
+    -webkit-appearance: none;
+    width: 20px;
+    height: 20px;
+    background-image: radial-gradient(circle, #4934b0 40%, #162cd4);
+    border-radius: 50%;
+    box-shadow: 0px 0px 4px 2px rgba(0, 0, 0, 0.0);
+  }
+
+  ::-moz-range-thumb {
+    width: 24px;
+    height: 18px;
+    -moz-appearance: none;
+    background-image: radial-gradient(circle, #282895 40%, #271696 45%);
+    border-radius: 50%;
+    box-shadow: 0px 0px 4px 2px rgba(0, 0, 0, 0.5);
+  }
+`;
+
+export default PokeRange;
